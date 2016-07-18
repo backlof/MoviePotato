89ad562d4667b7b81f2e4f3f9010dbac4a1bca4c
backlof <backlof@github.com>
2016-07-18 14:10:56 +0200

    Refactor tests a little

--- Source/UnitTests/Functionality.cs
+++ Source/UnitTests/Functionality.cs
@@ -2,61 +2,57 @@
 using MoviePotato.Functionality;
 using MoviePotato.Models;
 using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
 
 namespace UnitTests
 {
-	[TestClass]
-	public class Functionality
-	{
-		private static readonly string DATABASE_TEST = "dbtest.xml";
-		private static readonly string DATABASE_NEW = "dbnew.xml";
-		private static readonly string DATABASE_NONE = "dbnone.xml";
-		private static readonly string FILENAME = "file.txt";
-		public string BaseDirectory { get { return System.AppDomain.CurrentDomain.BaseDirectory; } }
+    [TestClass]
+    public class Functionality
+    {
+        private static readonly string DATABASE_TEST = "dbtest.xml";
+        private static readonly string DATABASE_NEW = "dbnew.xml";
+        private static readonly string DATABASE_NONE = "dbnone.xml";
+        private static readonly string FILENAME = "file.txt";
 
-		public List<Video> Movies { get; set; }
-		public Video Video { get; set; }
+        public string BaseDirectory => System.AppDomain.CurrentDomain.BaseDirectory;
 
-		[TestMethod]
-		public void ShouldHandleNoDatabaseFile()
-		{
-			Assert.IsFalse(MovieExplorerReader.HasDatabase(DATABASE_NONE));
-		}
+        public Video Video { get; set; }
+        public List<Video> VideosFromNewDatabase { get; set; }
+        public List<Video> VideosFromTestDatabase { get; set; }
 
-		[TestMethod]
-		public void ShouldHandleNewDatabase()
-		{
-			Movies = MovieExplorerReader.GetAllVideos(DATABASE_NEW);
+        public Functionality()
+        {
+            VideosFromNewDatabase = MovieExplorerReader.GetAllVideos(DATABASE_NEW);
+            VideosFromTestDatabase = MovieExplorerReader.GetAllVideos(DATABASE_TEST);
+            Video = new Video() { Location = BaseDirectory, FileName = FILENAME, Watched = true };
+        }
 
-			Assert.IsTrue(MovieExplorerReader.HasDatabase(DATABASE_NEW));
-			Assert.AreEqual(Movies.Count, 0);
-			Assert.AreEqual(Movies.Unwatched().GetCount(), 0);
-		}
+        [TestMethod]
+        public void ShouldHandleNoDatabaseFile()
+        {
+            Assert.IsFalse(MovieExplorerReader.HasDatabase(DATABASE_NONE));
+        }
 
-		[TestMethod]
-		public void ShouldHandleDatabaseWithItems()
-		{
-			Movies = MovieExplorerReader.GetAllVideos(DATABASE_TEST);
+        [TestMethod]
+        public void ShouldHandleNewDatabase()
+        {
+            Assert.IsTrue(MovieExplorerReader.HasDatabase(DATABASE_NEW));
+            Assert.AreEqual(VideosFromNewDatabase.Count, 0);
+            Assert.AreEqual(VideosFromNewDatabase.Unwatched().GetCount(), 0);
+        }
 
-			Assert.IsTrue(MovieExplorerReader.HasDatabase(DATABASE_TEST));
-			Assert.AreEqual(Movies.Count, 2);
-			Assert.AreEqual(Movies.Unwatched().GetCount(), 1);
-			Assert.IsNotNull(Movies.Unwatched().Random());
-		}
+        [TestMethod]
+        public void ShouldHandleDatabaseWithItems()
+        {
+            Assert.IsTrue(MovieExplorerReader.HasDatabase(DATABASE_TEST));
+            Assert.AreEqual(VideosFromTestDatabase.Count, 2);
+            Assert.AreEqual(VideosFromTestDatabase.Unwatched().GetCount(), 1);
+            Assert.IsNotNull(VideosFromTestDatabase.Unwatched().Random());
+        }
 
-		[TestMethod]
-		public void ShouldFindFile()
-		{
-			Video = new Video()
-			{
-				Location = BaseDirectory,
-				FileName = FILENAME,
-				Watched = true
-			};
-
-			Assert.IsTrue(Video.HasLocalCopy);
-		}
-	}
+        [TestMethod]
+        public void ShouldFindFile()
+        {
+            Assert.IsTrue(Video.HasLocalCopy);
+        }
+    }
 }
